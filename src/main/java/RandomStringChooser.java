863751e10f5eb06ed21dc690d52c3f5d69617716
aljow <143014720+aljow@users.noreply.github.com>
2024-04-19 12:24:53 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -9,7 +9,7 @@ public class RandomStringChooser
     }
   }
   public String getNext() {
-    int randomNum = (int)(Math.random()*(arr.length+1));
+    int randomNum = (int)(Math.random()*(arr.size()+1));
     if (randomNum == arr.size()) {
       return "NONE";
     } 
